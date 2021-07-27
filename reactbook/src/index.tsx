41743ab454234a440d343410c65ddbbc4ba76154
Queentesa Fuggett <queentesa.fuggett@perficient.com>
2021-07-27 13:53:03 -0400

    add esbuild file to public to access to use in index, added react hooks

--- reactbook/src/index.tsx
+++ reactbook/src/index.tsx
@@ -1,13 +1,39 @@
-import { useState } from 'react';
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+    const ref = useRef<any>();
     const [input, setInput] = useState('');
     const [code, setCode] = useState('');
 
-    const onClick = () => {
-        console.log(input);
+
+    // goes into the public directory to retrieve the esbuild service to store inside the ref
+    const startService = async () => {
+        ref.current = await esbuild.startService({
+            worker: true,
+            wasmURL: '/esbuild.wasm'
+        });
     };
+
+    useEffect(() => {
+        startService();
+    }, []);
+
+    const onClick = async () => {
+        if (!ref.current) {
+            return;
+        }
+
+        // code that we want to transpile
+        const result = await ref.current.transform(input, {
+            loader: 'jsx',
+            target: 'es2015'
+        });
+
+        setCode(result.code);
+    };
+
     return <div>
         <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
         <div>
