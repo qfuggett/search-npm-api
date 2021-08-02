fe9c943271ba200316159aa4ac272c9d5b428c5f
Queentesa Fuggett <queentesa.fuggett@perficient.com>
2021-08-02 15:56:32 -0400

    install faker package, update user with faker data

---
+++ typescript-maps/src/user.ts
@@ -0,0 +1,19 @@
+// May get typescript warning messages for imports that do not include a type definition file
+import faker from 'faker';
+
+
+export class User {
+    name: string;
+    location: {
+        lat: number;
+        lng: number;
+    };
+
+    constructor() {
+        this.name = faker.name.firstName();
+        this.location = {
+            lat: parseFloat(faker.address.latitude()),
+            lng: parseFloat(faker.address.longitude())
+        };
+    }
+}
